de9d5fb4e5090f2729da79c9c4c701855161de1d
Sebastiaan Wouters <sebastiaanwouters@tutanota.com>
2024-12-16 15:35:48 +0100

    chore: use constants for connection options

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
 import { Proxy } from './lib';
 
-const proxy = new Proxy('localhost', 3307, 'localhost', 3306);
+const PROXY_PORT = 3307;
+const PROXY_URL = 'localhost';
+const MYSQL_PORT = 3306;
+const MYSQL_URL = 'localhost';
+
+const proxy = new Proxy(PROXY_URL, PROXY_PORT, MYSQL_URL, MYSQL_PORT);
