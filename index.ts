ed6a5fdacddf77d0ae0d3ce4bfbaaaea64f05127
Sebastiaan Wouters <sebastiaanwouters@tutanota.com>
2024-12-17 00:22:42 +0100

    update proxy with error handling + query support

--- index.ts
+++ index.ts
@@ -1,8 +1,81 @@
-import { Proxy } from './lib';
+import mysql, { type Connection } from 'mysql2';
 
 const PROXY_PORT = 3307;
 const PROXY_URL = 'localhost';
 const MYSQL_PORT = 3306;
 const MYSQL_URL = 'localhost';
 
-const proxy = new Proxy(PROXY_URL, PROXY_PORT, MYSQL_URL, MYSQL_PORT);
+
+const server = mysql.createServer((conn) => {
+    console.log('connection');
+});
+server.listen(PROXY_PORT);
+
+server.on('connection', (conn: Connection) => {
+  console.log('connection');
+
+  conn.serverHandshake({
+    protocolVersion: 10,
+    serverVersion: 'proxy',
+    connectionId: 1234,
+    statusFlags: 2,
+    characterSet: 8,
+    capabilityFlags: 0xffffff,
+  });
+
+  conn.on('field_list', (table, fields) => {
+    console.log('field list:', table, fields);
+    conn.writeEof();
+  });
+
+  const remote = mysql.createConnection({
+    user: 'stagesol',
+    database: 'employees',
+    host: MYSQL_URL,
+    password: 'stagesol',
+    port: MYSQL_PORT,
+  });
+
+  conn.on('query', (sql: string) => {
+    console.log(`proxying query: ${sql}`);
+    remote.query(sql, function (err, result, fields) {
+      // overloaded args, either (err, result :object)
+      // or (err, rows :array, columns :array)
+      console.log('result', JSON.stringify(result));
+      console.log('fields', JSON.stringify(fields));
+      console.log('err', JSON.stringify(err));
+ 
+      if (err) {
+        console.log('err', JSON.stringify(err));
+        conn.writeError(err);
+        return;
+      }
+
+      if (Array.isArray(result)) {
+        // response to a 'select', 'show' or similar
+        const rows = result;
+        const columns = fields;
+        console.log('rows', rows);
+        console.log('columns', columns);
+        conn.writeTextResult(rows, columns);
+      } else {
+        // response to an 'insert', 'update' or 'delete'
+        console.log('result', JSON.stringify(result));
+        conn.writeOk(result);
+      }
+    });
+  });
+
+  conn.on('end', () => {
+    console.log('connection end');
+    if (conn) {
+        conn.end();
+    }
+  });
+  conn.on('quit', () => {
+    console.log('conn quit');
+    if (conn) {
+        conn.end();
+    }
+  });
+});
--- lib/proxy.ts
+++ lib/proxy.ts
@@ -1,90 +1,79 @@
-import type { Socket, TCPSocketListener } from 'bun';
-import { getBufferContent, getParsedSQLQuery, processReturnData } from './utils';
+import mysql from 'mysql2';
 
 export class Proxy {
-  private clientSocket: TCPSocketListener | null;
-  private mysqlSocket: Socket | null;
-  private clientOpenSocket: Socket | null;
   private readonly REMOTE_URL: string;
   private readonly REMOTE_PORT: number;
   private readonly CLIENT_URL: string;
   private readonly CLIENT_PORT: number;
-
+ 
   constructor(clientUrl: string, clientPort: number, remoteUrl: string, remotePort: number) {
-    this.clientSocket = null;
-    this.mysqlSocket = null;
-    this.clientOpenSocket = null;
     this.REMOTE_URL = remoteUrl;
     this.REMOTE_PORT = remotePort;
     this.CLIENT_URL = clientUrl;
     this.CLIENT_PORT = clientPort;
     this.newClientConn(this.CLIENT_URL, this.CLIENT_PORT);
+    
   }
 
-  public async newMysqlConn(url: string, port: number): Promise<Socket> {
-    const socket = await Bun.connect({
-      hostname: url,
-      port: port,
-      socket: {
-        data: (socket, data) => {
-          if (!this.clientSocket) {
-            console.error('received mysql data but client socket not connected');
-            return;
-          }
-          processReturnData(data);
-          this.pipeFromRemote(this.clientOpenSocket, socket, data);
-        },
-        close: () => {
-          console.log('mysql socket closed');
-        },
-      },
+  private newClientConn(url: string, port: number) {
+    const remote = mysql.createPool({
+      user: 'stagesol',
+      database: 'stagesol',
+      host: '127.0.0.1',
+      password: 'stagesol',
     });
-    return socket;
-  }
+    const server = mysql.createServer((conn) => {
+      console.log('New MySQL connection');
 
-  public async newClientConn(url: string, port: number): Promise<void> {
-    console.log('Listening on: ', url, port);
-    const socket = await Bun.listen({
-      hostname: url,
-      port: port,
-      socket: {
-        open: async (socket) => {
-          console.log('client socket connected');
-          this.clientOpenSocket = socket;
-          this.mysqlSocket = await this.newMysqlConn(this.REMOTE_URL, this.REMOTE_PORT);
-          console.log('mysql socket connected');
-        },
-        data: async (socket, data) => {
-          if (!this.mysqlSocket) {
-            console.error('received client data but mysql socket not connected');
-            return;
+      conn.serverHandshake({
+        protocolVersion: 10,
+        serverVersion: 'proxy',
+        connectionId: 1234,
+        statusFlags: 2,
+        characterSet: 8,
+        capabilityFlags: 0xffffff,
+      });
+
+      conn.on('error', (err) => {
+        console.error('MySQL connection error:', err);
+      });
+
+      conn.on('close', () => {
+        console.log('MySQL connection closed');
+      });
+
+      conn.on('query', (query) => {
+        console.log('MySQL query:', query);
+        remote.query(query, function (err) {
+          // conn.sequenceId = 1;
+          // overloaded args, either (err, result :object)
+          // or (err, rows :array, columns :array)
+          if (Array.isArray(arguments[1])) {
+            // response to a 'select', 'show' or similar
+            const rows = arguments[1],
+            columns = arguments[2];
+            console.log('rows', rows);
+            console.log('columns', columns);
+            conn.writeTextResult(rows, columns);
+          } else {
+            // response to an 'insert', 'update' or 'delete'
+            const result = arguments[1];
+            console.log('result', result);
+            conn.writeOk(result);
           }
-          this.pipeFromClient(socket, this.mysqlSocket, data);
-        },
-      },
-    });
-    this.clientSocket = socket;
+        });
+      });
+
+    })
+
+    server.listen(this.CLIENT_PORT);
+    console.log('MySQL server listening on port 3307');
+
+    
   }
 
-  public async pipeFromClient(clientSocket: Socket, remoteSocket: Socket, data: Buffer): Promise<void> {
-    if (!clientSocket || !remoteSocket) return;
-    const bufferData = getBufferContent(data);
-    const query = getParsedSQLQuery(bufferData);
-    if (query) {
-      console.log('received query: ', query);
-    }
-    remoteSocket.write(data);
-  }
-
-  public async pipeFromRemote(clientSocket: Socket | null, remoteSocket: Socket | null, data: Buffer): Promise<void> {
-    if (!clientSocket || !remoteSocket) return;
-    clientSocket.write(data);
-  }
-
-  public async getMysqlResponse(data: Buffer): Promise<Buffer | void> {
-    if (!this.clientSocket || !this.mysqlSocket) return;
-    this.mysqlSocket.write(data);
-    return data;
+  private newMysqlConn(url: string, port: number) {
+    
   }
 
 }
