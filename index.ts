7b332ea76e12bb00e0d2f2a3640c6d93bb4b27f2
Sebastiaan Wouters <sebastiaanwouters@tutanota.com>
2024-12-17 02:00:43 +0100

    fixes

--- index.ts
+++ index.ts
@@ -3,13 +3,14 @@ import mysql, { type Connection } from 'mysql2';
 const PROXY_PORT = 3307;
 const PROXY_URL = 'localhost';
 const MYSQL_PORT = 3306;
-const MYSQL_URL = 'localhost';
-
+const MYSQL_URL = 'mysql';
 
 const server = mysql.createServer((conn) => {
     console.log('connection');
 });
 server.listen(PROXY_PORT);
+
+console.log('proxy listening on port', PROXY_PORT);
 
 server.on('connection', (conn: Connection) => {
   console.log('connection');
@@ -28,12 +29,28 @@ server.on('connection', (conn: Connection) => {
     conn.writeEof();
   });
 
-  const remote = mysql.createConnection({
+  const remote = mysql.createPool({
     user: 'stagesol',
-    database: 'employees',
+    database: 'stagesol',
     host: MYSQL_URL,
     password: 'stagesol',
     port: MYSQL_PORT,
+    multipleStatements: true,
+    typeCast: (field, next) => {
+      if (field.type === 'TINY') {
+        return field.string();
+      }
+      if (field.type === 'LONG') {
+        return field.string();
+      }
+      if (field.type === 'BLOB') {
+        return field.string();
+      } else if (field.type === 'DATETIME') {
+        const date = field.string();
+        return date;
+      }
+      return next();
+    }
   });
 
   conn.on('query', (sql: string) => {
@@ -41,9 +58,6 @@ server.on('connection', (conn: Connection) => {
     remote.query(sql, function (err, result, fields) {
       // overloaded args, either (err, result :object)
       // or (err, rows :array, columns :array)
-      console.log('result', JSON.stringify(result));
-      console.log('fields', JSON.stringify(fields));
-      console.log('err', JSON.stringify(err));
  
       if (err) {
         console.log('err', JSON.stringify(err));
@@ -57,19 +71,28 @@ server.on('connection', (conn: Connection) => {
         const columns = fields;
         console.log('rows', rows);
         console.log('columns', columns);
+        conn.sequenceId = 1;
         conn.writeTextResult(rows, columns);
       } else {
         // response to an 'insert', 'update' or 'delete'
-        console.log('result', JSON.stringify(result));
+        conn.sequenceId = 1;
         conn.writeOk(result);
       }
     });
   });
 
+  conn.on('close', () => {
+    console.log('connection close');
+  });
+
+  conn.on('error', (err) => {
+    console.log('error', err);
+  });
+
   conn.on('end', () => {
     console.log('connection end');
     if (conn) {
-        conn.end();
+        conn.destroy();
     }
   });
   conn.on('quit', () => {
