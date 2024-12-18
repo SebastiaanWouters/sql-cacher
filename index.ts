c83166ed5487e517b5c5ea787fd5df74a960d5e4
Sebastiaan Wouters <sebastiaan.wouters@kuleuven.be>
2024-12-18 14:25:03 +0100

    initialize deno project

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { Proxy } from './lib';
-
-const PROXY_PORT = 3307;
-const PROXY_URL = 'localhost';
-const MYSQL_PORT = 3306;
-const MYSQL_URL = 'localhost';
-
-const proxy = new Proxy(PROXY_URL, PROXY_PORT, MYSQL_URL, MYSQL_PORT);
--- lib/index.ts
+++
@@ -1,4 +0,0 @@
-// import and export all the files in the lib directory
-import { Proxy } from "./proxy";
-
-export { Proxy };
